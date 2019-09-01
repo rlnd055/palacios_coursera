15a20468b0e51601e379e695ae0657e20a19c759
Rolando <rlndplcs@gmail.com>
2019-09-01 02:08:19 +0000

    Adding code outline and documdntation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -2,27 +2,25 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file stats.c 
+ * @file stats.c
  * @brief Unsigned char data analysis
  *
  * These are a set of functions that can analyze an array of unsigned char items
- * and report analytics on the maximum, minimum, mean and median of the data set. 
- * The data set is then reordered from large to small. The statistics are rounded  
+ * and report analytics on the maximum, minimum, mean and median of the data set.
+ * The data set is then reordered from large to small. The statistics are rounded
  * down to the nearest integer. Finally, the data is properly printed to the screen.
  *
  * @author Rolando Palacios
  * @date Aug. 31, 2019
  *
  */
-
-
 
 #include <stdio.h>
 #include "stats.h"
@@ -40,8 +38,35 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  /* Invoking Statistics and Printing Functions Go Here */
 
 }
 
-/* Add other Implementation File Code Here */
+void print_statistics(unsigned char * arr, unsigned int size){
+
+};
+
+void print_array(unsigned char * arr, unsigned int size){
+
+};
+
+unsigned char find_median(unsigned char * arr, unsigned int size){
+
+};
+
+unsigned char find_mean(unsigned char * arr, unsigned int size){
+
+};
+
+unsigned char find_maximum(unsigned char * arr, unsigned int size){
+
+};
+
+unsigned char find_minimum(unsigned char * arr, unsigned int size){
+
+};
+
+void sort_array(unsigned char * arr, unsigned int size){
+
+};
+
--- stats.h
+++ stats.h
@@ -2,10 +2,10 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
@@ -21,20 +21,104 @@
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
-
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints the statistics of an array
  *
- * <Add Extended Description Here>
+ * Given an pointer to an array and its size, prints the
+ * minimum, maximum, mean and median of the array data.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param ptr Pointer to data array
+ * @param size Size of the array
  *
- * @return <Add Return Informaiton here>
+ * @return void
  */
 
+void print_statistics(unsigned char * arr, unsigned int size);
+
+/**
+ * @brief Prints array to the screen
+ *
+ * Given a pointer to an array and its size,
+ * prints the array data to the screen.
+ *
+ * @param ptr Pointer to data array
+ * @param size Size of the array
+ *
+ * @return void
+ */
+
+void print_array(unsigned char * arr, unsigned int size);
+
+/**
+ * @brief Finds the median of an array
+ *
+ * Given a pointer to an array and its size,
+ * returns the median value of the array data
+ * rounded down to the nearest integer.
+ *
+ * @param ptr Pointer to data array
+ * @param size Size of the array
+ *
+ * @return unsigned char
+ */
+
+unsigned char find_median(unsigned char * arr, unsigned int size);
+
+/**
+ * @brief Finds the mean of an array
+ *
+ * Given a pointer to an array and its size,
+ * returns the mean value of the array data
+ * rounded down to the nearest integer.
+ *
+ * @param ptr Pointer to data array
+ * @param size Size of the array
+ *
+ * @return unsigned char
+ */
+
+unsigned char find_mean(unsigned char * arr, unsigned int size);
+
+/**
+ * @brief Finds the maximum of an array
+ *
+ * Given a pointer to an array and its size,
+ * returns the maximum value of the array data.
+ *
+ * @param ptr Pointer to data array
+ * @param size Size of the array
+ *
+ * @return unsigned char
+ */
+
+unsigned char find_maximum(unsigned char * arr, unsigned int size);
+
+/**
+ * @brief Finds the minimum of an array
+ *
+ * Given a pointer to an array and its size,
+ * returns the minimum value of the array data.
+ *
+ * @param ptr Pointer to data array
+ * @param size Size of the array
+ *
+ * @return unsigned char
+ */
+
+unsigned char find_minimum(unsigned char * arr, unsigned int size);
+
+/**
+ * @brief Sorts an array
+ *
+ * Given an pointer to an array and its size, sorts
+ * the array from largest to smallest.
+ *
+ * @param ptr Pointer to data array
+ * @param size Size of the array
+ *
+ * @return void
+ */
+
+void sort_array(unsigned char * arr, unsigned int size);
 
 #endif /* __STATS_H__ */
